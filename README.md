d5aba7690e9aa4080ff3249f26c89673c3962f65
nerdgrass <arboerstler@gmail.com>
2014-11-16 18:48:02 -0500

    hello world with ui router

--- README.md
+++ README.md
@@ -8,7 +8,18 @@ Sketchbook is a client-side javascript application that documents my experiments
     - NPM
 * Build System
     - Gulp
+    - Livereload
+    - Express
+    - Browserify
+* Framework
+    - Angular
 
+
+## Tutorials used
+* Using Browserify w/ Gulp
+    - http://maximilianschmitt.me/posts/angular-angular-ui-browserify-tutorial/
+* Angular, Gulp, & Browserify
+    - http://mindthecode.com/lets-build-an-angularjs-app-with-browserify-and-gulp/
 
 *Documentation last updated on:*
 *11/16/2014*
--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,9 +1,30 @@
 'use strict';
 
-var angular = require('angular');
+var angular = require('angular'),
+    uiRouter = require('angular-ui-router');
 
-var app = angular.module('sketchbook', [] );
+// Primary module & dependencies
+var app = angular.module('sketchbook', ['ui.router'] );
 
-app.controller('HelloCtrl', function($scope) {
+// State Management
+app.config(function($stateProvider) {
+  $stateProvider 
+  // Home
+  .state('main', {
+    url: '/',
+    templateUrl: './views/main.html'
+  });
+});
+app.config(['$urlRouterProvider',
+  function($urlRouterProvider) {
+    $urlRouterProvider.otherwise('/');
+  }
+]);
+
+// Controllers
+app.controller('MainCtrl', function($scope) {
+});
+
+app.controller('TestCtrl', function($scope) {
   $scope.test = 'lol';
 });
