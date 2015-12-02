58a4628fd2c1c5f29ea82b14352e5f31a97ab7d1
nerdgrass <arboerstler@gmail.com>
2015-12-02 10:20:36 -0600

    add webpack and build server

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+/*
+  Webpack Config
+
+  Webpack config is a POJO (Plain Ol' Javascript
+  Object) that defines the transpilation of our
+  app into browser-optimized files.
+*/
+
+// Dependencies
+var path = require('path');
+var webpack = require('webpack');
+
+// Webpack Config for web
+module.exports = {
+  entry: [
+    './src/js/index',
+    'webpack-dev-server/client?http://localhost:3000'
+  ],
+  devtool: 'eval',
+  output: {
+      path: path.join(__dirname, 'dist'),
+      publicPath: '/dist/',
+      filename: 'index.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.css$/,
+        loader: "style!css!autoprefixer"
+      },
+      {
+        test: /\.jsx?$/,
+        include: path.join(__dirname, 'src'),
+        loader: 'babel?presets[]=react,presets[]=es2015'
+      },
+    ]
+  }
+};
